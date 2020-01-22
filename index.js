5ae403f5c42d5a80225c2ea12bb3fa725b31a7e2
Simon Fishel <sfishel@splunk.com>
2020-01-21 21:14:19 -0800

    got the temperature sensor working

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 const SmartApp = require('@smartthings/smartapp');
 
+const { NODE_ENV } = process.env;
+
 const smartapp = new SmartApp()
   .publicKey('@smartthings_rsa.pub')
-  .enableEventLogging(2) // logs all lifecycle event requests and responses as pretty-printed JSON. Omit in production
   .configureI18n()
   .page('mainPage', (context, page, configData) => {
-    page.section('sensors', section => {
+    page.section('tempSensor', section => {
       section
-        .deviceSetting('contactSensor')
-        .capabilities(['contactSensor'])
-        .required(false);
+        .deviceSetting('tempSensor')
+        .capabilities(['temperatureMeasurement'])
+        .required(true)
+        .permissions('r');
+      section
+        .numberSetting('setPoint')
+        .required(true);
     });
-    page.section('lights', section => {
+    page.section('thermostat', section => {
       section
-        .deviceSetting('lights')
+        .deviceSetting('thermostat')
         .capabilities(['switch'])
-        .multiple(true)
-        .permissions('rx');
+        .permissions('rx')
+        .required(true);
     });
+  })
+  .updated(async (context, updateData) => {
+    await context.api.subscriptions.unsubscribeAll()
+    return context.api.subscriptions.subscribeToDevices(context.config.tempSensor, 'temperatureMeasurement', 'temperature', 'myDeviceEventHandler');
+  })
+  .subscribedEventHandler('myDeviceEventHandler', async (context, event) => {
+    const temp = event.value;
+    const { thermostat, setPoint } = context.config;
+    const thermostatId = thermostat && thermostat.length > 0 && thermostat[0].deviceConfig.deviceId;
+    if (thermostatId) {
+        const fanState = await context.api.devices.getAttributeValue(thermostatId, 'switch', 'switch');
+        if (temp > setPoint && fanState === 'off') {
+          console.log('[LOOK HERE] Turning the fan on because the temp is', temp);
+          return context.api.devices.sendCommands(thermostat, 'switch', 'on');
+        } 
+        if (temp < setPoint && fanState === 'on') {
+          console.log('[LOOK HERE] Turning the fan off because the temp is', temp);
+          return context.api.devices.sendCommands(thermostat, 'switch', 'off');
+        }
+    }
   });
+
+if (NODE_ENV !== 'production') {
+    smartapp.enableEventLogging(2);
+}
 
 exports.index = (req, res) => {
   req.url = '/st-handler';
